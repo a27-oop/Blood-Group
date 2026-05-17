a22f198e3c791c05a2d664f5d3c541f6c45ece37
Ahammed Musa <noushed18@gmail.com>
2026-05-17 12:48:50 +0600

    Auntication_system-creation

--- settings.py
+++ settings.py
@@ -1,6 +1,14 @@
+from lifelink.settings import BASE_DIR
+
+
 INSTALLED_APPS = [
     
     'accounts',
     'donor',
     'requestsystem',
     'ai_validation',]
+
+STATIC_URL = 'static/'
+
+STATICFILES_DIRS = [ BASE_DIR / "static"
+]
