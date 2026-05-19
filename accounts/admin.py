e11eb2aa93b8870a817f9b98052f1fbb5e5b3da3
Ahammed Musa <noushed18@gmail.com>
2026-05-19 21:34:09 +0600

    button_creation

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
-from .models import UserRegistration
+from .models import UserRegistration, EmergencyRequest
 
 admin.site.register(UserRegistration)
+admin.site.register(EmergencyRequest)
