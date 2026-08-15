3d0cba76095ba8fc2bcbb0ff2bd8c8ff05a1cf96
jannatul <jannatul@manarat.ac.bd>
2026-08-15 22:26:36 +0600

    Fix static files for production

--- lifelink/settings.py
+++ lifelink/settings.py
@@ -117,12 +117,15 @@ USE_TZ = True
 
 
 # Static files (CSS, JavaScript, Images)
-# https://docs.djangoproject.com/en/6.0/howto/static-files/
+
 
 STATIC_URL = '/static/'
 
 STATICFILES_DIRS = [
     BASE_DIR / "static",
 ]
+
+STATIC_ROOT = BASE_DIR / "staticfiles"
+
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
